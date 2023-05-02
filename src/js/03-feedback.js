9db9863a28ab4c9147cfe87bb8c831a925a99712
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2023-05-02 14:48:00 +0300

    final edits

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,7 +6,8 @@ const formEl = document.querySelector('.feedback-form');
 
 const STORAGE_KEY = 'feedback-form-state';
 
-let formData = {};
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 formEl.addEventListener('input', throttle(onFormInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
@@ -21,9 +22,10 @@ function onFormSubmit(ev) {
   }
 
   console.log(formData);
-
+ 
   ev.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(STORAGE_KEY);
   formData = {};
 }
 function onFormInput(ev) {
@@ -34,13 +36,9 @@ function onFormInput(ev) {
 function populateForm() {
   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (savedData.email) {
-    inputEl.value = savedData.email;
-    formData.email = savedData.email;
-  }
-
-  if (savedData.message) {
-    textareaEl.value = savedData.message;
-    formData.message = savedData.message;
+  if (savedData) {
+    inputEl.value = savedData.email || " ";
+    textareaEl.value = savedData.message || " ";
+    
   }
 }
